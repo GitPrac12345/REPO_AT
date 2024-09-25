67ae14a09e558a31b4f35c5f3ed0d88d51fd66d0
GitPrac12345 <gitgitdemoprac@gmail.com>
2024-09-25 22:45:12 +0530

    Login commit

---
+++ src/test/java/testcases/TC_002_LoginTest.java
@@ -0,0 +1,19 @@
+package testcases;
+
+public class TC_002_LoginTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("UserName");
+		System.out.println("Password");
+		System.out.println("Login");
+
+
+		
+		
+	}
+	
+	
+	
+}
