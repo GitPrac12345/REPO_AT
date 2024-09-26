e225aef5b279af3c769e2d92ac82f495b2a95de1
GitPrac12345 <gitgitdemoprac@gmail.com>
2024-09-26 22:21:00 +0530

    jj

--- src/test/java/testcases/TC_002_LoginTest.java
+++ src/test/java/testcases/TC_002_LoginTest.java
@@ -3,50 +3,29 @@ package testcases;
 public class TC_002_LoginTest {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("UserName");
 		System.out.println("Password");
 		System.out.println("Login");
-		
-		
-		
-		//Updated Login
-		
-		
+
+		// Updated Login
+
 		System.out.println("UserName1");
 		System.out.println("Password1");
 		System.out.println("Login1");
-		
-		
-		//Updated Login
-		
-		
-				System.out.println("UserName2");
-				System.out.println("Password2");
-				System.out.println("Login2");
-				
-				
-				//Updated Login
-				
-				
-						System.out.println("UserName3");
-						System.out.println("Password3");
-						System.out.println("Login3");
-				
-	
-	//Updated Login4 New
-				
-				
-	System.out.println("UserName4");
-	System.out.println("Password4");
-	System.out.println("Login4");
-							
-		
-		
-		
+
+		// Updated Login
+
+		System.out.println("UserName2");
+		System.out.println("Password2");
+		System.out.println("Login2");
+
+		// Updated Login
+
+		System.out.println("UserName3");
+		System.out.println("Password3");
+		System.out.println("Login3");
+
 	}
-	
-	
-	
+
 }
